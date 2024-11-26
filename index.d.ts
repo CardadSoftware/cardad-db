859c5994f758ab0946c928e62edeb3b5360aa2d9
Anthony Stoute <crackpotmgee@gmail.com>
2024-11-25 21:17:03 -0500

    update schemas

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,13 @@
+import { Types } from 'mongoose';
 export declare class JobModel{
+    _id: Types.ObjectId;
     jobName: String;
     invoices?: [any];
     customer: UserModel
 }
 
 export declare class UserModel{
+    _id: Types.ObjectId;
     username: String;
     firstName: String;
     lastName: String;
